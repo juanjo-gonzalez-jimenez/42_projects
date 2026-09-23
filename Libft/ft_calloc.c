bd1576f2c3903fe1cf56313be7ab453c251df6a1
Juan Jose Gonzalez Jimenez <320576248+juanjo-gonzalez-jimenez@users.noreply.github.com>
2026-09-23 17:07:36 +0200

    finished calloc

--- Libft/ft_calloc.c
+++ Libft/ft_calloc.c
@@ -6,7 +6,21 @@
 /*   By: juan-jos <juan-jos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/23 16:22:44 by juan-jos          #+#    #+#             */
-/*   Updated: 2026/09/23 16:22:45 by juan-jos         ###   ########.fr       */
+/*   Updated: 2026/09/23 17:04:34 by juan-jos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void	*calloc(size_t nmemb, size_t size)
+{
+	void	*ptr; //pointer to the allocated memory
+	size_t	total_size; //total size of the memory to be allocated
+
+	total_size = nmemb * size; //total size of the memory to be allocated
+	ptr = malloc(total_size); //memory assignated to the pointer
+	if (!ptr)
+		return (NULL);
+	ft_bzero(ptr, total_size); //converting all the bits in the memory to 0
+	return (ptr); //return pointer
+}
