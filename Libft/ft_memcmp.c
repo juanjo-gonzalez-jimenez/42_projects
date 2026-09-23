561f45f5f105213b06ebbc96eb911b0095717ca5
Juan Jose Gonzalez Jimenez <juan-jos@c3r6s6.42malaga.com>
2026-09-23 12:42:56 +0200

    completed 2 new functions and modified the libft.h

--- Libft/ft_memcmp.c
+++ Libft/ft_memcmp.c
@@ -6,13 +6,29 @@
 /*   By: juan-jos <juan-jos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/22 18:02:50 by juan-jos          #+#    #+#             */
-/*   Updated: 2026/09/22 18:05:22 by juan-jos         ###   ########.fr       */
+/*   Updated: 2026/09/23 12:00:09 by juan-jos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int memcmp(const void *s1, const void *s2, size_t n)
+int ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-    
+	const unsigned char *ptr1;
+	const unsigned char *ptr2;
+
+	ptr1 = (const unsigned char *)s1;
+	ptr2 = (const unsigned char *)s2;
+
+	while (n > 0)
+	{
+		if (*ptr1 != *ptr2)
+		{
+			return (*ptr1 - *ptr2);
+		}
+		ptr1++;
+		ptr2++;
+		n--;
+	}
+	return (0);
 }
